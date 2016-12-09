6b82ff9f74bdc38996fa73313566de40d324d795
Samuel Littley <samuel.littley@toastwaffle.com>
2016-12-09 23:19:37 +0000

    Add custom base model class
    Change-Id: Ic9d85579858040f500fad2c8a515304433afd626

---
+++ lime/database/custom_model.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+"""Base table schema to reduce duplication."""
+
+import flask_sqlalchemy
+import sqlalchemy
+
+from . import errors
+
+
+class CustomModel(flask_sqlalchemy.Model):
+  """Base table schema to reduce duplication."""
+  __tablename__ = None
+
+  object_id = sqlalchemy.Column(
+    sqlalchemy.Integer(),
+    primary_key=True,
+    nullable=False
+  )
+
+  def __repr__(self):
+    return '<{0}({1})>'.format(self.__class__.__name__, self.object_id)
+
+  @classmethod
+  def get_by(cls, field, value):
+    """Get an object by a unique database field."""
+    item = cls.query.filter(field == value).first()
+
+    if not item:
+      raise errors.ObjectNotFoundError(
+        'No {name} found with {field} = {value}'.format(
+          name=cls.__name__,
+          field=field.name,
+          value=value))
+
+    return item
--- lime/database/db.py
+++ lime/database/db.py
@@ -2,11 +2,19 @@
 """Create the SQLAlchemy connection object."""
 
 import flask_sqlalchemy
+from sqlalchemy.ext import declarative
 
 from .. import app
+from . import custom_model
 
 # The config isn't loaded when the database object is created. Setting this
 # configuration value suppresses a warning given by SQLAlchemy.
 app.APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 DB = flask_sqlalchemy.SQLAlchemy(app.APP)
+
+# Awkward hack to use a custom model class. Flask-SQLAlchemy 3.0 (in beta as of
+# 2016-01-11) has a model_class parameter to the above constructor, which should
+# be used once v3.0 is released as stable.
+DB.Model = declarative.declarative_base(cls=custom_model.CustomModel)
+DB.Model.query = flask_sqlalchemy._QueryProperty(DB) # pylint: disable=protected-access
