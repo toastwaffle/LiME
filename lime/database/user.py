c3aa73cff542c27a0d9d5498c3280863e4c82d41
Samuel Littley <samuel.littley@toastwaffle.com>
2017-03-11 10:34:24 +0000

    Add magic constructor for models

--- lime/database/user.py
+++ lime/database/user.py
@@ -9,11 +9,7 @@ class User(DB.Model):
   """Model for users."""
   __tablename__ = 'user'
 
+  # Fields
   name = DB.Column(DB.Unicode(200), nullable=False)
   email = DB.Column(DB.Unicode(200), nullable=False, unique=True)
   password_hash = DB.Column(DB.Unicode(60), nullable=False)
-
-  def __init__(self, name, email, password_hash):
-    self.name = name
-    self.email = email
-    self.password_hash = password_hash
