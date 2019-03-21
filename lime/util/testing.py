b0c51243c3dd83c059a61a435ff23a0bd8c5d700
Samuel Littley <samuel.littley@toastwaffle.com>
2019-03-21 15:22:14 +0000

    Add a utility to generate a request with a token

--- lime/util/testing.py
+++ lime/util/testing.py
@@ -1,10 +1,22 @@
 """Setup for testing."""
 
+import typing
+
 import contextlib
 
 from lime import app
 from lime.database import db
+from lime.database import models
 from lime.system import setup
+from lime.util import api
+from lime.util import auth
+
+# pylint: disable=unused-import,ungrouped-imports,invalid-name
+if typing.TYPE_CHECKING:
+  from typing import (
+      Any,
+      Dict,
+  )
 
 
 @contextlib.contextmanager
@@ -16,3 +28,9 @@ def test_setup():
     setup.configure_app('testing')
     db.DB.create_all()
     yield client
+
+
+def with_token(request: 'Dict[str, Any]') -> 'str':
+  """Build a JSON request with a valid token."""
+  return api.ENCODER.encode(
+      dict(request, token=auth.JWT.from_user(models.User(object_id=1))))
