b6367bdb099dcc5e97ec47cabcbb3f2c52e61774
Samuel Littley <samuel.littley@toastwaffle.com>
2019-03-22 11:33:39 +0000

    Refactor crossdomain library to remove unneeded complexity
    We only call this in one place, so remove all of the parameters we don't use

--- lime/util/crossdomain.py
+++ lime/util/crossdomain.py
@@ -21,52 +21,32 @@ if typing.TYPE_CHECKING:
 # pylint: enable=unused-import,ungrouped-imports,invalid-name
 
 
+_MAX_AGE = '21600'
+
+
 def allow(
     origins: 'List[str]',
-    methods: 'Optional[List[str]]' = None,
-    headers: 'Optional[List[str]]' = None,
-    max_age: int = 21600,
-    attach_to_all: bool = True,
-    automatic_options: bool = True) -> 'Callable[[Callable], Callable]':
+    headers: 'List[str]') -> 'Callable[[Callable], Callable]':
   """Create a decorator to allow cross-domain requests using Access-Control."""
   origins = ', '.join(origins)
-
-  if methods is not None:
-    methods = ', '.join(sorted(x.upper() for x in methods))
-
-  if headers is not None:
-    headers = ', '.join(x.upper() for x in headers)
-
-  if isinstance(max_age, datetime.timedelta):
-    max_age = max_age.total_seconds()
-
-  def get_methods() -> 'List[str]':
-    """Get allowed HTTP methods for Access-Control-Allow-Methods."""
-    if methods is not None:
-      return methods
-
-    options_resp = flask.current_app.make_default_options_response()
-    return options_resp.headers['allow']
+  headers = ', '.join(x.upper() for x in headers)
 
   def decorator(f: 'Callable') -> 'Callable':
     """The decorator."""
     def wrapped_function(*args: 'Any', **kwargs: 'Any'):
       """The wrapped function."""
-      if automatic_options and flask.request.method == 'OPTIONS':
-        resp = flask.current_app.make_default_options_response()
+      opts_resp = flask.current_app.make_default_options_response()
+
+      if flask.request.method == 'OPTIONS':
+        resp = opts_resp
       else:
         resp = flask.make_response(f(*args, **kwargs))
 
-      if not attach_to_all and flask.request.method != 'OPTIONS':
-        return resp
+      resp.headers['Access-Control-Allow-Origin'] = origins
+      resp.headers['Access-Control-Allow-Headers'] = headers
+      resp.headers['Access-Control-Allow-Methods'] = opts_resp.headers['allow']
+      resp.headers['Access-Control-Max-Age'] = _MAX_AGE
 
-      h = resp.headers
-
-      h['Access-Control-Allow-Origin'] = origins
-      h['Access-Control-Allow-Methods'] = get_methods()
-      h['Access-Control-Max-Age'] = str(max_age)
-      if headers is not None:
-        h['Access-Control-Allow-Headers'] = headers
       return resp
 
     f.provide_automatic_options = False
